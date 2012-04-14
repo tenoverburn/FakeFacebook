990f6b044418165b2e50b300fcf40ff2f74329db
tenoverburn <tenoverburn@gmail.com>
2012-04-14 13:59:30 +0100

    facebook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FakeFacebook::Application.config.secret_token = '514f3ba5c1716c77c006e77a075f7e50652f5a3f4e36e319d9879326d6ec4c8b1ff32b802b124342fa8f6db87db476db1e1427cbf3b6057aa08921fd4f580c7d'
